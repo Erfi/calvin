dc0bd34583a715e2c6411153d2cc04df29b0bef4
Erfan Azad <erfan.basiri.azad@gmail.com>
2025-08-12 20:18:22 +0200

    float to list in NormalizeVector transform input

--- calvin_models/calvin_agent/utils/transforms.py
+++ calvin_models/calvin_agent/utils/transforms.py
@@ -19,6 +19,10 @@ class NormalizeVector(object):
     """Normalize a tensor vector with mean and standard deviation."""
 
     def __init__(self, mean=0.0, std=1.0):
+        if isinstance(mean, float):
+            mean = [mean]
+        if isinstance(std, float):
+            std = [std]
         self.std = torch.Tensor(std)
         self.std[self.std == 0.0] = 1.0
         self.mean = torch.Tensor(mean)
